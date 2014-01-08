42bcef48f5cbe5e75761d7f197e166f3d6b6ed0d
Karl Purkhardt <karl.purkhardt@gmail.com>
2014-01-08 20:56:40 +0000

    Added socket.io to server

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require('express'),
     app = express(),
-    server;
+    server,
+    io;
 
 // Configure express app to use static file server
 app.use(express.static(__dirname + '/'));
 
 // Create a new http server
 server = require('http').createServer(app);
+
+// Attach socket.io to the http server
+io = require('socket.io').listen(server);
 
 // Start the server
 server.listen(8080);
