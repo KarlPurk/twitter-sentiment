7295dea2a73a991786faf5043985ee53828f36a2
Karl Purkhardt <karl.purkhardt@gmail.com>
2014-02-14 17:20:25 +0000

    Added tweet table to home page and added basic bootstrap styles.

--- app/app.js
+++ app/app.js
@@ -1,25 +1,52 @@
 (function(global) {
+
     var app = new Marionette.Application();
+
     app.addRegions({
 		main: 'main'
     });
-    app.module('tweets', function() {
+
+    app.module('tweets', function(tweetsModule, app) {
 		var tweets = new Backbone.Collection();
-		var TweetView = Marionette.ItemView.extend({
-			template: "#tweet-template"
+        var TweetView = Marionette.ItemView.extend({
+            tagName: 'tr',
+			template: "#tweet-template",
+            templateHelpers: {
+                getSentimentClass: function() {
+                    if (this.sentiment.type === 'positive') {
+                        return 'label-success';
+                    }
+                    else if (this.sentiment.type === 'negative') {
+                        return 'label-danger';
+                    }
+                    return 'label-info';
+                }
+            }
 		});
-		var TweetsView = Marionette.CollectionView.extend({
-			itemView: TweetView
+        var TweetsView = Marionette.CompositeView.extend({
+            className: 'white-box',
+            template: "#tweets-template",
+            itemView: TweetView,
+            itemViewContainer: 'tbody'
 		});
-		var tweetsView = new TweetsView({
+        var tweetsView = new TweetsView({
 			collection: tweets 
 		});
-		var socket = io.connect('http://localhost');
-		socket.emit('filter', { track: 'javascript' });
-		socket.on("tweet", function(tweet) {
-			console.log(tweets.add(tweet));
-		});
-		app.getRegion('main').show(tweetsView);
+        tweetsModule.addInitializer(function() {
+            var socket = io.connect('http://localhost');
+            socket.emit('filter', { track: 'javascript' });
+            socket.on("tweet", function(tweet) {
+                tweets.add(tweet);
+                // TODO: Dev only.  Remove at some point in time!
+                if (tweets.length == 5) {
+                    socket.disconnect();
+                }
+            });
+        });
+        tweetsModule.addInitializer(function() {
+            app.getRegion('main').show(tweetsView);
+        });
     });
+
     global.app = app;
 })(window);
